11fce8ec84c0b4737497ff29329c9ea6c9bbdfcb
Chris156728 <107707089+Chris156728@users.noreply.github.com>
2022-06-23 15:06:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,6 +40,11 @@ if(!isMobileDevice()){
 	// Grab the connected wallet address, if available, to pass into the Login component
      walletAddress = web3props.accounts ? web3props.accounts[0] : "";
 }
+if(isMobileDevice() && window.ethereum){
+	contractAvailable = !(!web3props.web3 && !web3props.accounts && !web3props.contract);
+	// Grab the connected wallet address, if available, to pass into the Login component
+     walletAddress = web3props.accounts ? web3props.accounts[0] : "";
+}		
 	return (
 		<div className="App">
 			<Router>
